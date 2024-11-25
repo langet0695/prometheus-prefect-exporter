d2c181ab264f33913771e5f29d21fcdcb5fb1150
tlange <tlange@qualtrics.com>
2024-11-25 15:23:49 -0800

    adding utils folder and calculator

---
+++ utils/auth.py
@@ -0,0 +1,22 @@
+import base64
+
+
+class Authorization:
+    def __init__(self, api_key, api_user, api_password):
+        self.api_key = api_key
+        self.api_user = api_user
+        self.api_password = api_password
+
+    def get_auth_header(self):
+        auth_type = "Unset"
+        auth_value = ""
+        if self.api_key:
+            auth_type = "Bearer Token"
+            auth_value = f"Bearer {self.api_key}"
+        elif self.api_user and self.api_password:
+            auth_type = "Basic Auth"
+            auth_str = f"{self.api_user}:{self.api_password}"
+            b64_auth_str = base64.b64encode(bytes(auth_str, "utf-8")).decode("utf-8")
+            auth_value = f"Basic {b64_auth_str}"
+        # logger.info("Authorization Type: %s", auth_type)
+        return auth_value, auth_type
