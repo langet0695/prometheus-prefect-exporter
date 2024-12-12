70cb3c20fe07f210b5db65558de0066701e5b253
tlange <tlange@qualtrics.com>
2024-12-12 13:41:19 -0800

    switching error to info

--- metrics/work_queues.py
+++ metrics/work_queues.py
@@ -80,7 +80,7 @@ class PrefectWorkQueues(PrefectApiMetric):
             except requests.exceptions.HTTPError as err:
                 self.logger.error(err)
                 if retry >= self.max_retries - 1:
-                    self.logger.info(
+                    self.logger.error(
                         "%s max retries of %d reached", self.uri, self.max_retries
                     )
                     time.sleep(1)
